c5c0abbc93aecb1412e7dd9e5a7d693dfd8d4213
auriceliof <auricelio.suporte@hotmail.com>
2026-02-09 20:12:13 -0300

    Base Project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import './App.css'
 
-export default function App() {
+function App() {
+  
   return (
-      <h1>PROJETO MODELO</h1>
-  );
+    {}
+  )
 }
+
+export default App
