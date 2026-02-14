10927b083be1adb4a2db7999a00ad7cf321882d3
auriceliof <auricelio.suporte@hotmail.com>
2026-02-14 11:18:05 -0300

    Add filter button

--- frontend/src/pages/HomePage/MainSection.jsx
+++ frontend/src/pages/HomePage/MainSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ProductsContainer from './ProductsContainer'
+import ProductFilters from './ProductFilters'
 
 const MainSection = () => {
   return (
     <>
-     <h1>Filtros</h1>
+     <ProductFilters />
      <ProductsContainer />
     </>
   )
