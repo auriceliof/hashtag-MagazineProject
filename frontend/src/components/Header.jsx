3fa4bc4d549810da198b4eef8058d68b550d916b
auriceliof <auricelio.suporte@hotmail.com>
2026-02-14 11:31:24 -0300

    Add Car Number

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import UserButtons from "./UserButtons";
 
 const Header = () => {
   return (
-    <header className="flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-base z-10">
+    <header className="flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-xl z-10">
       <a href="/">
         <img className='h-16 px-2' src={logo} alt="Logo da Hashtag." />
       </a>
