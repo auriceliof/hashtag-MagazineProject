c644cee3d35ad677883e025fd4837e13490a09fa
auriceliof <auricelio.suporte@hotmail.com>
2026-02-14 10:58:02 -0300

    Add button car

--- frontend/src/pages/HomePage/ProductCard.jsx
+++ frontend/src/pages/HomePage/ProductCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
 const ProductCard = ({ id, name, brand, image, feminine, price }) => {
   return (
@@ -7,6 +9,9 @@ const ProductCard = ({ id, name, brand, image, feminine, price }) => {
       <p className='mx-4 text-sm text-slate-400 '>{brand}</p>
       <p className='mx-4 text-sm'>{name}</p>
       <p className='mx-4 text-sm text-green-700'>{price}</p>
+      <button className='bg-slate-950 rounded-md mx-4 my-1 text-slate-100 hover:bg-slate-800 '>
+        <FontAwesomeIcon icon={faCartPlus}/>
+      </button>
     </article>
   )
 }
--- frontend/src/pages/HomePage/ProductsContainer.jsx
+++ frontend/src/pages/HomePage/ProductsContainer.jsx
@@ -7,6 +7,7 @@ const ProductsContainer = () => {
     <section className='container flex flex-wrap mx-auto p-10 justify-center gap-10'>
         {catalog.map(product => (
             <ProductCard 
+                key={`product_${product.id}_key`}
                 {...product}
             />
         ))}
