f01aeb3f65577cb59f2c17e9b35e29c824396e1f
auriceliof <auricelio.suporte@hotmail.com>
2026-02-15 17:56:11 -0300

    Add Link from buttons

--- frontend/src/components/UserButtons.jsx
+++ frontend/src/components/UserButtons.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 const UserButtons = () => {
   return (
@@ -9,9 +10,9 @@ const UserButtons = () => {
         <FontAwesomeIcon icon={faCartShopping} />
         <div id='cart-amount' className='absolute inline-flex items-center justify-center w-6 h-6 font-bold text-sm text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2'>3</div>
       </button>
-      <a className='px-2'>
-        <FontAwesomeIcon icon={faUser} /> 
-      </a>
+      <Link to='/history' >
+        <FontAwesomeIcon className='px-2' icon={faUser} /> 
+      </Link>
     </div>
   )
 }
