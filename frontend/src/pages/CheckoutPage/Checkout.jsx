56acdc32ca7a70b5ffd6a771b9767bde4a3d7cae
auriceliof <auricelio.suporte@hotmail.com>
2026-03-15 08:46:59 -0300

    style checkout page

--- frontend/src/pages/CheckoutPage/Checkout.jsx
+++ frontend/src/pages/CheckoutPage/Checkout.jsx
@@ -1,5 +1,11 @@
 const Checkout = () => {
-  return <h1>Bem-vindo à página de checkout</h1>;
+  return (
+    <main className="bg-stone-200 h-[calc(100dvh-5rem)]">
+      <p className="text-center text-2xl font-bold text-slate-950 pt-8">
+        Finalizar Compra
+      </p>
+    </main>
+  );
 };
 
 export default Checkout;
