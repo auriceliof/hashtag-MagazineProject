72f89fff15057d5bacda27208d2ea0468129119e
auriceliof <auricelio.suporte@hotmail.com>
2026-03-15 08:57:35 -0300

    Add grid in chackout page

--- frontend/src/pages/CheckoutPage/Checkout.jsx
+++ frontend/src/pages/CheckoutPage/Checkout.jsx
@@ -4,6 +4,14 @@ const Checkout = () => {
       <p className="text-center text-2xl font-bold text-slate-950 pt-8">
         Finalizar Compra
       </p>
+      <form className="grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 h-3/4 mt-5 px-8">
+        <p className="bg-blue-700">Elemento 1</p>
+        <p className="bg-blue-700">Elemento 2</p>
+        <p className="bg-blue-700">Elemento 3</p>
+        <p className="bg-blue-700">Elemento 4</p>
+        <p className="bg-blue-700">Elemento 5</p>
+        <p className="bg-blue-700">Elemento 6</p>
+      </form>
     </main>
   );
 };
